ad061382314d4094f1dd063f6389d02c9a2a67c5
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2023-08-28 12:32:30 -0600

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import partytown from "@astrojs/partytown";
 import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), react()],
+  experiments: {
+    react: true
+  }
 });
